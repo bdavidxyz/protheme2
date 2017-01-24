6c7a5b4ecfd9981d8ce4f9449bb0fb5fdfc0dea7
David <davbohr@gmail.com>
2017-01-24 13:49:50 +0100

    stuffs

--- gulpfile.js
+++ gulpfile.js
@@ -77,6 +77,8 @@ gulp.task('watch', function () {
                 'js/*.js',
                 '*.html',
                 '*.md',
+                '_drafts/*.html',
+                '_drafts/*.md',
                 '_layouts/*.html',
                 '_includes/*.html',
                 '_includes/*.svg',
